1fff8c96463e19073ff564623c798d780cb4480c
espark-alim <ali@esparkconsultants.com>
2023-07-03 20:03:57 +0500

    minor changes

--- app.js
+++ app.js
@@ -40,35 +40,52 @@ var questions = [
     option: ["read only memory", "hyper text markup language", "html", "html"],
     correctAns: "read only memory",
   },
+  
 ];
 
 var display = document.getElementById("display");
 var option = document.getElementById("option");
 var current = document.getElementById("current");
 var total = document.getElementById("total");
-var indexValue = [0];
+var indexValue = 0;
 var marks = 0;
+var obtained = document.getElementById("obtained");
+
 function render() {
   var obj = questions[indexValue];
   display.innerHTML = questions[indexValue].question;
   current.innerHTML = indexValue + 1;
   total.innerHTML = questions.length;
   option.innerHTML = "";
+   
   for (let i = 0; i < obj.option.length; i++) {
     // console.log(obj.option[i]);
-    option.innerHTML += `<button type="button" class="btn btn-primary w-100 my-2" onclick="passData('${obj.correctAns}', '${obj.option[i]}')" >${obj.option[i]}</button>`;
+    option.innerHTML += `<input type="radio" name = "value" id = "input" class="mx-3 my-3" onclick="passData('${obj.correctAns}', '${obj.option[i]}')" >${obj.option[i]} </br>`;
   }
 }
 render();
 
 function passData(a, b) {
   if (a == b) {
-    console.log("right Ans");
     marks++;
-  }
-  console.log(marks);
+    // console.log(marks);
 }
+}
+document.getElementById("next").addEventListener("click",next)
+// document.getElementById("input").addEventListener("change", chk);
+
+function chk(){
+   console.log("onchange")
+}
+
 function next() {
   indexValue++;
   render();
+  if (total.innerHTML == 5) {
+    obtained.innerHTML = +marks
+    console.log(marks);
+  }
+
 }
+
+console.log(id)
